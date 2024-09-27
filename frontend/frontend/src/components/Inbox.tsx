55f37328a27290798d15a5a33595068580345d5e
abhishekbrinks <131379199+abhishekbrinks@users.noreply.github.com>
2024-09-27 22:31:40 +0530

    "Updated InboxComponent: added loading state, changed email filter from 'Microsoft' to 'Alia Bhatt', and refactored JSX structure and styling."

--- frontend/frontend/src/components/Inbox.tsx
+++ frontend/frontend/src/components/Inbox.tsx
@@ -250,9 +250,10 @@ const InboxComponent: React.FC = () => {
   const [emails, setEmails] = useState<Record<string, any[]>>({});
   const [expandedAuthors, setExpandedAuthors] = useState<{ [key: string]: boolean }>({});
   const [synced, setSynced] = useState(false);
-
+  const [loading, setLoading] = useState(true);
   // Function to be called when button is clicked
   const handleButtonClick = () => {
+    setLoading(true)
     setEmails({})
     setSynced((prev) => !prev); // Toggle state to trigger useEffect
   };
@@ -283,13 +284,20 @@ const InboxComponent: React.FC = () => {
   useEffect(() => {
     async function fetchAndSetEmails() {
       try {
-        const combinedEmails = await fetchEmails({containsFilter:"Microsoft"});
+        const combinedEmails = await fetchEmails({ containsFilter: "Alia Bhatt" });
         setEmails(combinedEmails);
       } catch (error) {
         console.error(error);
       }
     }
     fetchAndSetEmails();
+
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 5000); // 3 seconds
+
+    return () => clearTimeout(timeoutId);
+
   }, [synced]);
 
   const toggleExpandEmail = (author: string) => {
@@ -300,71 +308,6 @@ const InboxComponent: React.FC = () => {
   };
 
   return (
-    //     <div className="py-2 bg-gray-900 text-white">
-    //       <div className="space-y-4 bg-gray-800 p-6 rounded-lg shadow-lg h-[90vh] overflow-y-auto">
-    //         <div className="flex items-center space-x-2 text-2xl font-semibold">
-    //           {token && <FaGoogle className="text-blue-400" />}
-    //           {token_ms && <FaMicrosoft className="text-green-400" />}
-    //           <h3>Emails </h3>
-    //         </div>
-
-    //         {Object.keys(emails).length > 0 ? (
-    //           Object.keys(emails).map((author) => (
-    //             <div key={author} className={`bg-slate-700 p-4 rounded-lg shadow border-l-4 ${emails[author][0].source === 'Gmail' ? 'border-blue-400' : 'border-green-400'}`}>
-    //               <div className="flex justify-between items-center mb-2">
-    //                 <div>
-    //                   <h4 className="font-semibold text-lg text-white">{author}</h4>
-    //                   <div className="flex items-center space-x-4">
-    //                     <p className="text-sm text-gray-400">{emails[author].length} email(s)</p>
-    //                     <p className="text-sm text-gray-400">{convertTimestampToDate(emails[author][0].date)}</p>
-    //                   </div>
-    //                 </div>
-    //                 <button onClick={() => toggleExpandEmail(author)} className="text-blue-400 font-semibold">
-    //                   {expandedAuthors[author] ? 'Collapse' : 'Expand'}
-    //                 </button>
-    //               </div>
-
-    //               {/* Emails from this author */}
-    //               {expandedAuthors[author] && (
-    //                 <div className="space-y-2 mt-4">
-    //                   {emails[author].map((email) => (
-    //                     <div style={{backgroundColor:'rgb(23 29 75)'}} key={email.id} className={` p-3 rounded-lg flex items-start border-l-4 ${email.source === 'Gmail' ? 'border-blue-400' : 'border-green-400'}`}>
-    //                       {/* Icon for Gmail or Outlook */}
-    //                       {email.source === 'Gmail' ? (
-    //                         <FaGoogle className="text-blue-400 mr-2" />
-    //                       ) : (
-    //                         <FaMicrosoft className="text-green-400 mr-2" />
-    //                       )}
-    //                       <div className="flex-1">
-    //                         {/* Display read/unread status for Gmail */}
-    //                         {email.source === 'Gmail' && (
-    //                           <p className={`text-sm font-semibold ${checkReadStatus(email.labelIds) === "UNREAD" ? "text-red-400" : "text-emerald-500"}`}>
-    //                             {capitalizeFirstLetter(checkReadStatus(email.labelIds))}
-    //                           </p>
-    //                         )}
-    //                         <h5 className="font-semibold text-md">{email.subject}</h5>
-    //                         <p className="text-sm text-gray-300">{email.snippet}</p>
-    //                       </div>
-    //                     </div>
-    //                   ))}
-    //                 </div>
-    //               )}
-    //             </div>
-    //           ))
-    //         ) : (
-
-
-    //           <div role="status" className='flex items-center justify-center h-[90%]'>
-    //     <svg aria-hidden="true" className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-    //         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-    //         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    //     </svg>
-    //     <span className="sr-only">Loading...</span>
-    // </div>
-
-    //         )}
-    //       </div>
-    //     </div>
     <div className="py-2 bg-gray-900 text-white">
       <div className="space-y-4 bg-gray-800 px-6 pb-6 rounded-lg shadow-lg h-[90vh] overflow-y-auto">
 
@@ -382,101 +325,104 @@ const InboxComponent: React.FC = () => {
         </div>
 
         {/* Scrollable emails section */}
-        {Object.keys(emails).length > 0 ? (
-          Object.keys(emails).map((author) => (
-            <div
-              key={author}
-              className={`bg-slate-700 p-4 rounded-lg shadow border-l-4 ${emails[author][0].source === 'Gmail'
+        {Object.keys(emails).length > 0 ?
+          (
+            Object.keys(emails).map((author) => (
+              <div
+                key={author}
+                className={`bg-slate-700 p-4 rounded-lg shadow border-l-4 ${emails[author][0].source === 'Gmail'
                   ? 'border-blue-400'
                   : 'border-green-400'
-                }`}
-            >
-              <div className="flex justify-between items-center mb-2">
-                <div>
-                  <div className="flex row gap-2 items-center">
-                {emails[author][0].source === 'Gmail'
-                  ? <FaGoogle className="text-blue-400" />
-                  : <FaMicrosoft className="text-green-400" />
-                }
-                
-                  <h4 className="font-semibold text-lg text-white">{author}</h4></div>
-                  <div className="flex items-center space-x-4">
-                    <p className="text-sm text-gray-400">
-                      {emails[author].length} email(s)
-                    </p>
-                    <p className="text-sm text-gray-400">
-                      {convertTimestampToDate(emails[author][0].date)}
-                    </p>
-                  </div>
-                </div>
-                <button
-                  onClick={() => toggleExpandEmail(author)}
-                  className="text-blue-400 font-semibold"
-                >
-                  {expandedAuthors[author] ? 'Collapse' : 'Expand'}
-                </button>
-              </div>
+                  }`}
+              >
+                <div className="flex justify-between items-center mb-2">
+                  <div>
+                    <div className="flex row gap-2 items-center">
+                      {emails[author][0].source === 'Gmail'
+                        ? <FaGoogle className="text-blue-400" />
+                        : <FaMicrosoft className="text-green-400" />
+                      }
 
-              {/* Emails from this author */}
-              {expandedAuthors[author] && (
-                <div className="space-y-2 mt-4">
-                  {emails[author].map((email) => (
-                    <div
-                      style={{ backgroundColor: 'rgb(23 29 75)' }}
-                      key={email.id}
-                      className={`p-3 rounded-lg flex items-start border-l-4 ${email.source === 'Gmail'
+                      <h4 className="font-semibold text-lg text-white">{author}</h4></div>
+                    <div className="flex items-center space-x-4">
+                      <p className="text-sm text-gray-400">
+                        {emails[author].length} email(s)
+                      </p>
+                      <p className="text-sm text-gray-400">
+                        {convertTimestampToDate(emails[author][0].date)}
+                      </p>
+                    </div>
+                  </div>
+                  <button
+                    onClick={() => toggleExpandEmail(author)}
+                    className="text-blue-400 font-semibold"
+                  >
+                    {expandedAuthors[author] ? 'Collapse' : 'Expand'}
+                  </button>
+                </div>
+
+                {/* Emails from this author */}
+                {expandedAuthors[author] && (
+                  <div className="space-y-2 mt-4">
+                    {emails[author].map((email) => (
+                      <div
+                        style={{ backgroundColor: 'rgb(23 29 75)' }}
+                        key={email.id}
+                        className={`p-3 rounded-lg flex items-start border-l-4 ${email.source === 'Gmail'
                           ? 'border-blue-400'
                           : 'border-green-400'
-                        }`}
-                    >
-                      {/* Icon for Gmail or Outlook */}
-                      {email.source === 'Gmail' ? (
-                        <FaGoogle className="text-blue-400 mr-2" />
-                      ) : (
-                        <FaMicrosoft className="text-green-400 mr-2" />
-                      )}
-                      <div className="flex-1">
-                        {/* Display read/unread status for Gmail */}
-                        {email.source === 'Gmail' && (
-                          <p
-                            className={`text-sm font-semibold ${checkReadStatus(email.labelIds) === 'UNREAD'
+                          }`}
+                      >
+                        {/* Icon for Gmail or Outlook */}
+                        {email.source === 'Gmail' ? (
+                          <FaGoogle className="text-blue-400 mr-2" />
+                        ) : (
+                          <FaMicrosoft className="text-green-400 mr-2" />
+                        )}
+                        <div className="flex-1">
+                          {/* Display read/unread status for Gmail */}
+                          {email.source === 'Gmail' && (
+                            <p
+                              className={`text-sm font-semibold ${checkReadStatus(email.labelIds) === 'UNREAD'
                                 ? 'text-red-400'
                                 : 'text-emerald-500'
-                              }`}
-                          >
-                            {capitalizeFirstLetter(checkReadStatus(email.labelIds))}
-                          </p>
-                        )}
-                        <h5 className="font-semibold text-md">{email.subject}</h5>
-                        <p className="text-sm text-gray-300">{email.snippet}</p>
+                                }`}
+                            >
+                              {capitalizeFirstLetter(checkReadStatus(email.labelIds))}
+                            </p>
+                          )}
+                          <h5 className="font-semibold text-md">{email.subject}</h5>
+                          <p className="text-sm text-gray-300">{email.snippet}</p>
+                        </div>
                       </div>
-                    </div>
-                  ))}
-                </div>
-              )}
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))
+          ) :
+          loading ? (
+            <div role="status" className="flex items-center justify-center h-[90%]">
+              <svg
+                aria-hidden="true"
+                className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>              
             </div>
-          ))
-        ) : (
-          <div role="status" className="flex items-center justify-center h-[90%]">
-            <svg
-              aria-hidden="true"
-              className="inline w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
-              viewBox="0 0 100 101"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                fill="currentColor"
-              />
-              <path
-                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                fill="currentFill"
-              />
-            </svg>
-            <span className="sr-only">Loading...</span>
-          </div>
-        )}
+          ) : (
+            <span className="">Seems like filter yielded no results</span>
+          )}
       </div>
     </div>
 
